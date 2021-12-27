002fd3c1567a6c429e7895c21c7a848fe9455c3e
mydevth <mydevth@chonburi.biz>
2021-12-27 23:09:25 +0700

    176 connected Firestore db

--- Section16-2/src/index.js
+++ Section16-2/src/index.js
@@ -1,5 +1,19 @@
 import{ initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
 
+//import { update } from 'lodash';
+
+// import {
+//     getAuth,
+//     createUserWithEmailAndPassword,
+//     signOut, signInWithEmailAndPassword,
+//     onAuthStateChanged
+// } from 'firebase/auth'
+
+//--------------------------------------------------
+//init firebase app
 const firebaseConfig = {
     apiKey: "AIzaSyAwtfkYNMElyrWLYYzCTDOSYqMucwn4zwI",
     authDomain: "javascript-section16.firebaseapp.com",
@@ -8,5 +22,22 @@ const firebaseConfig = {
     messagingSenderId: "195981413411",
     appId: "1:195981413411:web:d749a983d95239dc007596"
   };
-  
+
   initializeApp(firebaseConfig)
+
+  //-------------------------------------
+
+  //init services
+const db = getFirestore()
+
+//const auth = getAuth()
+
+//collection Ref
+const colRef = collection(db, 'recipes')
+
+//get collection data
+getDocs(colRef)
+  .then((snapshot)=> {
+      console.log(snapshot.docs)
+  })
+
