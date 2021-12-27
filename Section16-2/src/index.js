34286bba21dca17b5f3db7ddcf9a522ad2028d90
mydevth <mydevth@chonburi.biz>
2021-12-27 22:37:40 +0700

    Firebase9-175

--- Section16-2/src/index.js
+++ Section16-2/src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello from index.js');
+const firebaseConfig = {
+    apiKey: "AIzaSyAwtfkYNMElyrWLYYzCTDOSYqMucwn4zwI",
+    authDomain: "javascript-section16.firebaseapp.com",
+    projectId: "javascript-section16",
+    storageBucket: "javascript-section16.appspot.com",
+    messagingSenderId: "195981413411",
+    appId: "1:195981413411:web:d749a983d95239dc007596"
+  };
+  
